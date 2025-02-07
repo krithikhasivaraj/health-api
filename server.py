df893e85e629c196704bbc9870b2218796033cf5
krithikhasivaraj <116083347+krithikhasivaraj@users.noreply.github.com>
2025-02-07 19:50:17 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -85,4 +85,5 @@ def get_all_users_data():
         return jsonify({"error": f"Database query error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
